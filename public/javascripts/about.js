18f7fc21d1362887166fbf0a6e314253fac5a852
evelo2 <paul.thomas777@gmail.com>
2020-11-24 14:25:00 -0800

    Added About

---
+++ public/javascripts/about.js
@@ -0,0 +1,7 @@
+import { loadNavHTML } from './navLoader.js';
+
+// Set our page load
+document.addEventListener('DOMContentLoaded', () => {
+    // Set off the nav load.
+    loadNavHTML();
+});
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Demo Site' });
 });
 
+/* Also Handle the About page */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
 module.exports = router;
