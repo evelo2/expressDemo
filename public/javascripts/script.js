fadfa1420bd06a58121ec5b5fc1e29ecbe9438fe
evelo2 <paul.thomas777@gmail.com>
2020-11-25 21:13:49 -0800

    Added Filter

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -8,16 +8,31 @@ const personTemplate = ({ name, age }) => `
     </div>
 `;
 
+const loadPeopleUI = (people) => {
+    const html = people.map(person => personTemplate(person))
+    .join('');
+
+    document.querySelector('.people-list').innerHTML = html;
+};
+
 // Load the person list
 const loadPeople = async () => {
     const res = await fetch('people');
     const json = await res.json();
+    document.people = json;
+    loadPeopleUI(document.people);
+ };
 
-    const html = json.map(person => personTemplate(person))
-                     .join('');
-
-    document.querySelector('.people-list').innerHTML = html;
-};
+ const filterPeople = () => {
+    const { value: filter } = document.querySelector('#filter');
+    let filteredPeople = document.people;
+    if(filter != '') {
+        filteredPeople = filteredPeople.filter(({ hobbies }) =>{
+            return hobbies.some(hobby => hobby.toLowerCase().indexOf(filter.toLowerCase()) > -1);
+        });
+    }
+    loadPeopleUI(filteredPeople);
+ };
 
 // Set our page load
 document.addEventListener('DOMContentLoaded', () => {
@@ -26,4 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Set of the load People
     loadPeople();
+
+    document.querySelector('#btnfilter')
+            .addEventListener('click', ({ target }) => filterPeople());
 });
--- server/people.js
+++ server/people.js
@@ -1,22 +1,27 @@
 const people = [
     {
         name: "Paul",
+        hobbies: ['wood', 'food', 'gaming', 'code'],
         age: 21
     },
     {
         name: "Izzy",
+        hobbies:['garden', 'food', 'sewing', 'code'],
         age: 22
     },
     {
         name: "Molly",
+        hobbies: ['food', 'cuddles'],
         age: 3
     },
     {
         name: "Ben",
+        hobbies: ['food'],
         age: 12
     },
     {
         name: "BauBau",
+        hobbies: ['food', 'barking'],
         age: 12
     }
 ];
